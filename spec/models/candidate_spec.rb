6ce5d1f270d86880f028ed945ba541d010cc7515
ronimour <ronimour@gmail.com>
2013-11-27 20:29:50 -0300

    primeiras correções nos testes para aumentar a cobertura

--- spec/models/candidate_spec.rb
+++ spec/models/candidate_spec.rb
@@ -2,5 +2,8 @@
 require 'spec_helper'
 
 describe Candidate do
-  pending "add some examples to (or delete) #{__FILE__}"
+it "a candidate have to be authenticated" do
+    candidato = build(:candidate)
+    candidato.confirm("oi").should be == false
+  end
 end
